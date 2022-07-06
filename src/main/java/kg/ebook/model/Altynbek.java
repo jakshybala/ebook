f5b3e7a8d7f303e177d31ee03e0e164845fecbfe
Altynbek <altynbekzhumadiluulu@gmail.com>
2022-07-06 15:25:20 +0600

    altyndev

---
+++ src/main/java/kg/ebook/model/Altynbek.java
@@ -0,0 +1,17 @@
+package kg.ebook.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Altynbek {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+}
