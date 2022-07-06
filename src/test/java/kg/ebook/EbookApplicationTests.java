76aafac2e3a5566915f197fa096972f1ffa19ac5
jakshybala <maximuspro.kg@gmail.com>
2022-07-06 12:25:58 +0600

    start

---
+++ src/test/java/kg/ebook/EbookApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.ebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
