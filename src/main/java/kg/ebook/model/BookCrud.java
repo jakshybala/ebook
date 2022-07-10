0a74d43429e8e7c68844cb0a554a23e7367a4617
jakshybala <maximuspro.kg@gmail.com>
2022-07-10 17:47:22 +0600

    Book crud class

---
+++ src/main/java/kg/ebook/model/BookCrud.java
@@ -0,0 +1,14 @@
+package kg.ebook.model;
+
+/*
+kg.ebook.model
+Tarih: 06.07.2022, Saat: 12:28, Author: Grey 
+*/
+public class booktest {
+
+    public static void main(String[] args) {
+		private String name;
+		
+
+    }
+}
