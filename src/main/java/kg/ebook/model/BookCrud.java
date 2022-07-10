08161cbe75dc25231fd6dff5163afea471ab1e78
jakshybala <maximuspro.kg@gmail.com>
2022-07-10 21:07:55 +0600

    fix max

--- src/main/java/kg/ebook/model/BookCrud.java
+++ src/main/java/kg/ebook/model/BookCrud.java
@@ -4,11 +4,8 @@ package kg.ebook.model;
 kg.ebook.model
 Tarih: 06.07.2022, Saat: 12:28, Author: Grey 
 */
-public class booktest {
+public class BookCrud {
+    private String name;
 
-    public static void main(String[] args) {
-		private String name;
-		
 
-    }
 }
--- src/main/java/kg/ebook/model/max.java
+++
@@ -1,9 +0,0 @@
-package kg.ebook.model;
-
-public class NewClass {
-
-    public static void main(String[] args) {
-        System.out.println("Change file without Intelegi Idea");
-    }
-
-}
